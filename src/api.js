b6fc13d29f2dee08413601cc7a13de0518419aff
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2018-02-21 08:46:27 +0100

    implement wrap()

--- src/api.js
+++ src/api.js
@@ -1,11 +1,45 @@
 
-var DFG = {};
+var Node = require('./Node');
+var LiteralNode = require('./LiteralNode');
+var OperatorNode = require('./OperatorNode');
 
-DFG.LiteralNode = require('./LiteralNode');
-
-DFG.fromJSON = function(json) {
+function fromJSON(json) {
 
   return root;
+};
+
+function wrap(valueOrNode, properties) {
+  if (typeof valueOrNode == "number" || typeof valueOrNode == "string") {
+    return LiteralNode(valueOrNode, properties);
+  }
+
+  // TODO: we chould wrap some more well-known object types (e.g. DOM nodes)
+  // maybe this should be done in external code
+  // OR we could stick a hidden property to other objects (.__node) to track them,
+  // but this could also have side effects (iterating over properties etc.)
+
+  // return others unchanged
+  return valueOrNode;
+};
+
+function unwrap(valueOrNode) {
+
+};
+
+function isNode(valueOrNode) {
+  return valueOrNode instanceof Node;
 }
 
-module.exports = DFG;
+module.exports = {
+
+  // main API methods
+  wrap: wrap,
+  unwrap: unwrap,
+  isNode: isNode,
+
+  // node types
+  Node: Node,
+  LiteralNode: LiteralNode,
+  OperatorNode: OperatorNode
+
+};
